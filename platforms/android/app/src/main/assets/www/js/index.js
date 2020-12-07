1046a069d1e7abeacdec7ac063ad29aed6da1089
Timo604 <46822011+Timo604@users.noreply.github.com>
2020-12-07 04:15:10 -0600

    InfoWindow support
    Additionally some very very minor styling & config.xml updates with team info

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -24,6 +24,7 @@ var geocoder;
 var map;
 var service;
 document.addEventListener('deviceready', onDeviceReady, false);
+
 document.getElementById("search").addEventListener("click", function(){
 	var loc = document.getElementById("loc").value;
 	console.log('address is: ' + loc);
@@ -34,7 +35,7 @@ document.getElementById("search").addEventListener("click", function(){
 			alert("Please select a search radius");
 	}
 	else if(loc == ''){
-		alert("Please enter an address");
+		alert("Please enter a location");
 	}
 	else{
 	onSearch(loc, distance);	
@@ -59,24 +60,36 @@ function initMap(){
 		center: {lat: 41.8781, lng: -87.6298},
 		zoom: 8,});
 	
-	//add google autocomplete api to the location text bar for addresses
-    var options = {types: ['address']};
-    autocomplete = new google.maps.places.Autocomplete(document.getElementById("loc"), options);
+	//add google autocomplete api to the location text bar
+    //no options allows towns/cities/locations of interest to be used
+	autocomplete = new google.maps.places.Autocomplete(document.getElementById("loc"));
 	service = new google.maps.places.PlacesService(map);
 }
 
 function onSearch(address, distance){
 	//function to clear output of any previous results listed
 	clearOutput()
+	//adjusts map zoom based on search radius
+	//could use some refining for optimal UX, but a good start
+	if (distance = 5)
+	{
+		map.setZoom(11);
+	}
+	else if (distance = 15)
+	{
+		map.setZoom(7);
+	}
+	
 	var MeterDistance = distance * 1609.34; //Approximate miles to meters conversion
 	//geocoder converts address to latatude and longitude for the places API to use
 	geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
+		//add center marker of entered location  
+		var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
+		
 		//call to function that places the Google Places API request
 		placeSearch(results[0].geometry.location, MeterDistance);
       } else {
@@ -85,7 +98,8 @@ function onSearch(address, distance){
     });
   }
   
-  //Calls to Google Places API
+//Begins calls to Google Places API
+//nearby search to get places within the range
 function placeSearch(location, MeterDistance){
 	console.log("location: " + location);
 	console.log("MeterDistance: " + MeterDistance);
@@ -96,19 +110,20 @@ function placeSearch(location, MeterDistance){
 	};		
 	service.nearbySearch(request, detailSearch);
 }
-  
+//detailed search to get more information on those place
 function detailSearch(results, status){
 	if(status == google.maps.places.PlacesServiceStatus.OK)
 	  {
-		//display list that results get output to  
+		//display list that results get output to 
+		//change the display here, so it isn't updated with every gym result output in the next function
 		document.getElementById("output").style.display = "block";
 		results.forEach(function(value){
 			var request = {
 				placeId : value.place_id,
-				fields : ['name', 'formatted_address', 'website', 'geometry']
+				fields : ['name', 'formatted_address', 'website', 'geometry', 'formatted_phone_number', 'opening_hours']
 			};
 			service.getDetails(request, displayResults)
-			console.log("searching details...");
+			
 		});
 	  }
 	else
@@ -116,19 +131,61 @@ function detailSearch(results, status){
 		  alert('Place Search was not successful for the following reason: ' + status);
 	  }
 }
-	
+//displaying the places as markers and in the list
+//setting up event listeners for infoWindow
 function displayResults(place, status){
-	
-	console.log("to displaying results");
+	console.log("displaying results");
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		
+		//add marker to map
+		map.setCenter(place.geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+		//content string for the infoWindow
+		var contentString = place.name + "<br> Phone: " + place.formatted_phone_number;
+		//add infoWindow to marker
+		
+		var infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
 		var a = document.createElement("a");
 		a.href = place.website;
 		a.textContent = place.name;
 		var li = document.createElement("li");
-		li.appendChild(a)
+		li.appendChild(a);
+		
+		console.log(place.opening_hours.isOpen());
+		if(place.opening_hours.isOpen() != undefined && place.opening_hours.isOpen() == false)
+		{
+			var p = document.createElement("p");
+			p.textContent = "Closed";
+			p.style.color = "red";
+			li.appendChild(p);
+		}
+				
 		var ul = document.getElementById("output");
 		ul.appendChild(li);
+		var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+		
+		//add event listeners to the marker and the list
+		
+		marker.addListener('click', function(){
+			infoWindow.open(map, marker);
+		});
+		
+		li.addEventListener('click', function(){
+			infoWindow.open(map, marker);
+		});
+		
+		
+		
     }
+	
 }
 
 function clearOutput(){
