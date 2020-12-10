d4d10d745ab3a42bd17a61b6991ea6fd88cc5f5b
AlexanderKravets1 <44179950+AlexanderKravets1@users.noreply.github.com>
2020-12-09 21:13:05 -0600

    Delete imageFadder.js

--- www/src/js/imageFadder.js
+++
@@ -1,9 +0,0 @@
-var $imgs = $(".fader").find("img");
-var i = 0;
-
-function changeImage() {
-    var next = (++i % $imgs.length);
-    $($imgs.get(next -1)).fadeOut(500);
-    $($imgs.get(next)).fadeIn(500);
-}
-var interval = setInterval(changeImage, 2000);
